bad35eefe48d4c6a14e0c59f5060bdb3b5afe018
German Ramirez <mitsuominagi@gmail.com>
2021-06-24 11:19:16 -0500

    Fixed the Auth  Middleware

--- utils/professorAuth.js
+++ utils/professorAuth.js
@@ -1,9 +1,11 @@
 // Added the Middleware that will help us to redirect if the user is not logged on the system and one to access protected views
 const professorWithAuth = (req, res, next) => {  
-    if (!req.session.logged_in) {
+    if (!req.session.logged_in && req.session.logged_type==="professor") {
       res.redirect('/professorLogin');
-    } else {
+    } else if (req.session.logged_in && req.session.logged_type==="professor") {
       next();
+    }else{
+      res.redirect('/');
     }
   };
   
--- utils/studentAuth.js
+++ utils/studentAuth.js
@@ -1,9 +1,11 @@
 // Added the Middleware that will help us to redirect if the user is not logged on the system and one to access protected views
 const studentWithAuth = (req, res, next) => {  
-    if (!req.session.logged_in) {
+    if (!req.session.logged_in && req.session.logged_type==="student") {
       res.redirect('/studentLogin');
-    } else {
+    } else if (req.session.logged_in && req.session.logged_type==="student"){
       next();
+    }else{
+      res.redirect('/');
     }
   };
   
